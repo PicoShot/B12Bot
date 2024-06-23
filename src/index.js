2a60eaece413e80a8897d94027d4c4e07a93f2f2
PicoShot <116891491+PicoShot@users.noreply.github.com>
2024-06-23 21:58:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Partials, REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, REST, Routes, ActivityType } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
@@ -12,14 +12,49 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+    client.user.setPresence({
+        activities: [{ name: 'Owener => PicoShot', type: ActivityType.Playing }],
+        status: 'online',
+    });
     registerCommands();
 });
 
 const registerCommands = async () => {
     const commands = [
         {
-            name: 'hello',
-            description: 'Replies with a private message',
+            name: 'set_password',
+            description: 'Set a New Password',
+            options: [
+                {
+                    name: 'password_name',
+                    type: 3,
+                    description: 'Name The Password',
+                    required: true,
+                },
+                {
+                    name: 'password',
+                    type: 3,
+                    description: 'New Password',
+                    required: true,
+                },
+            ],
+        },
+        {
+            name: 'get_password',
+            description: 'Get Your Password',
+            options: [
+                {
+                    name: 'password_name',
+                    type: 3,
+                    description: 'The Password Name',
+                    required: true,
+                },
+            ],
+        },
+        {
+            name: 'whois',
+            description: 'Tell You Who u are ?',
+            options: [],
         },
     ];
 
@@ -42,10 +77,26 @@ const registerCommands = async () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    const { commandName, options } = interaction;
 
-    if (commandName === 'hello') {
-        await interaction.reply({ content: 'Hello!', ephemeral: true });
+    if (commandName === 'set_password') {
+
+        let passwordName = options.getString('password_name');
+        let password = options.getString('password');
+        let msg = `Debug:{${passwordName},${password}}Şifreni kaydettim! (bu sadece bir test mesajı hiç bi boku kaydetmedi.)`
+
+        await interaction.reply({ content: msg, ephemeral: true });
+    }
+    else if (commandName === 'get_password') {
+
+        let passwordName = options.getString('password_name');
+        let msg = `Debug:{${passwordName}}bi dur yarram botu daha yapmadık aq`
+
+        await interaction.reply({ content: msg, ephemeral: true });
+    }
+    else if (commandName === 'whois') {
+
+        await interaction.reply({ content: 'sen tam bir o.ç\'sun', ephemeral: true });
     }
 });
 
